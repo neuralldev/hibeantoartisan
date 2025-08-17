4dbfcd359ac93dba7133eb7ab45f1fe89cf2123d
neuralldev <thierry.gluzman@neurall.fr>
2025-08-17 17:59:08 +0200

    missing temperature

--- convert_hibean.py
+++ convert_hibean.py
@@ -469,7 +469,7 @@ class HibeanToArtisanConverter:
             alog_data["beans"] = roast_context.get("greenBeanWeight", {}).get("value", 0.0) if roast_context.get("greenBeanWeight") is not None else 0.0
             alog_data["weight"][0] = roast_context.get("greenBeanWeight", {}).get("value", 0.0) if roast_context.get("greenBeanWeight") is not None else 0.0
             alog_data["weight"][1] = roast_context.get("roastedBeanWeight", {}).get("value", 0.0) if roast_context.get("roastedBeanWeight") is not None else 0.0
-            alog_data["ambientTemp"] = roast_context.get("envTemp", {}).get("value", 0.0) if roast_context.get("ambientTemp") is not None else 0.0
+            alog_data["ambientTemp"] = roast_context.get("envTemp", {}).get("value", 0.0) if roast_context.get("envTemp") is not None else 0.0
             alog_data["ambient_humidity"] = roast_context.get("envHumidity", 0) if roast_context.get("envHumidity") is not None else 0
             alog_data["ambient_pressure"] = roast_context.get("pressure", 0) if roast_context.get("pressure") is not None else 0
 
@@ -594,8 +594,6 @@ class HibeanToArtisanConverter:
                 computed["DROP_BT"] = round(drop_event.get("temperature", 0.0), 1)
 
             computed["totaltime"] = round(hibean_data.get("roastContext", {}).get("duration", 0.0), 1) if roast_context.get("duration") is not None else 0.0
-            computed["ambient_temperature"] = round(roast_context.get("envTemp", {}).get("value", 0.0), 1) if roast_context.get("envTemp") is not None else 0.0
-            computed["ambient_humidity"] = round(roast_context.get("envHumidity", 0.0), 1) if roast_context.get("envHumidity") is not None else 0.0
 
             phases_map = {1: "dryphasetime", 2: "midphasetime", 3: "finishphasetime"}
             for phase_point in phase_list:
